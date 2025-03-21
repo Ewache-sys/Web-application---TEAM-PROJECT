5152061d7d8bcc74441ccf3431516e426060c49f
Ewache-sys <ewacheche@gmail.com>
2025-03-21 19:24:38 +0000

    Add files via upload

--- database.php
+++ database.php
@@ -1,26 +1,17 @@
 <?php
+// Database credentials
+$db_server = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "student_course_hub";
 
-    $db_server = "localhost";#
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "example-data";
-    $conn = "";
+// Attempt connection
+$conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
 
-try{
-    $conn = mysqli_connect( $db_server,  
-                            $db_user, 
-                            $db_password, 
-                            $db_name);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "You are connected! <br>";
 }
-catch(mysqli_sql_exception){
-    echo"Could not connect! <br>";
-}
-
-if($conn){
-        echo"You are connected! <br>";
-}
-else{
-    echo"Could not connect!";
-}
-
 ?>
