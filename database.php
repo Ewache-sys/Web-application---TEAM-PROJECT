e28d6246d6f923e42d3ee1293f3f3a56e8b512b8
Ewache-sys <ewacheche@gmail.com>
2025-03-25 19:26:44 +0000

    Update database.php

--- database.php
+++ database.php
@@ -1,17 +1,11 @@
 <?php
-// Database credentials
-$db_server = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "student_course_hub";
 
-// Attempt connection
-$conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "You are connected! <br>";
+$host="localhost";
+$user="root";
+$pass="";
+$db="login";
+$conn=new mysqli($host,$user,$pass,$db);
+if($conn->connect_error){
+    echo "Failed to connect DB".$conn->connect_error;
 }
 ?>
